8ff9bad8d8777f2bb2cd609b21a49727605e921c
UO266640 <jony23699@gmail.com>
2022-01-02 01:01:38 +0100

    Borrar producto

--- app/src/main/java/es/uniovi/eii/bestfood/PropiedadesActivity.java
+++ app/src/main/java/es/uniovi/eii/bestfood/PropiedadesActivity.java
@@ -2,20 +2,15 @@ package es.uniovi.eii.bestfood;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 public class PropiedadesActivity extends AppCompatActivity {
@@ -76,9 +71,7 @@ public class PropiedadesActivity extends AppCompatActivity {
                 mDatabase = FirebaseDatabase.getInstance().getReference();
                 id = FirebaseAuth.getInstance().getUid();
                 mDatabase.child("users").child(id).child("barcode").child(barcode).removeValue();
-
-                Intent intent2 = new Intent(this, MainActivity.class);
-               startActivity(intent2);
+                finish();
             }
         });
     }
